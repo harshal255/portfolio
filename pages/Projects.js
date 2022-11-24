fc4067290f4f7cf9ef11907b66922d8c5309f4d7
Harshal Kahar <harshalskahar389@gmail.com>
2022-11-24 18:12:30 +0530

    map bugs is fixed

--- pages/Projects.js
+++ pages/Projects.js
@@ -23,7 +23,8 @@ const Projects = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             {AllProjects.map((Projects) => {
-              return <>
+              
+              return <div key={Projects.id}>
                 <div className="p-4 md:w-1/3 ">
 
                   <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -57,7 +58,7 @@ const Projects = () => {
                   </div>
                 </div>
 
-              </>
+              </div>
             }).slice(0, 3)}
 
 
